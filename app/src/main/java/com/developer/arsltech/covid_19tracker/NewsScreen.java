1e98cfe658c547aadc58812a7b9c0f45ae627a76
CuongKV <cuongkv.it@gmail.com>
2021-05-09 15:19:35 +0700

    first commit

---
+++ app/src/main/java/com/developer/arsltech/covid_19tracker/NewsScreen.java
@@ -0,0 +1,14 @@
+package com.developer.arsltech.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_screen);
+    }
+}
